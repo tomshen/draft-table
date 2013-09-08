5e179b202f5c8dba1ae23b4890ce0ce06eca5b3f
Brooks Mershon <brooksmershon@gmail.com>
2013-09-08 00:54:41 -0400

    Switch to using js for migrations.js

---
+++ migrations.js
@@ -0,0 +1,149 @@
+var penn = {
+  name: "University of Pennsylvania",
+  email_domain: ".upenn.edu",
+  cover_image: "/public/img/penn_cover.jpg",
+  plans: []
+};
+
+var plan1 = {
+  title: "College House, Hill Square",
+  image_thumbnail: "/public/img/college_house_thumb.jpg",
+  supporters: [],
+  proposals: [],
+  elements: [
+    {
+      type: "text",
+      _class: "subheader",
+      content: "Planning Strategy"
+    }, {
+      type: "text",
+      _class: "",
+      content: "The New College House will establish a gateway to campus, as it is located at a key pedestrian approach from the northeastern corner of the University. It will be the first building one encounters coming from the 30th Street Amtrak and Regional Rail Station, and will front onto Woodland Walk, one of the three main pedestrian paths identified in the Campus Development Plan. A new public open space or commons will enliven the center of this important residential quadrangle."
+    }, {
+      type: "text",
+      _class: "subheader",
+      content: "Program"
+    }, {
+      type: "text",
+      _class: "",
+      content: "A new 300-400 bed College House, including residential advisors, faculty, and house master suites. The ground floor will feature public amenities such as dining and social spaces."
+    }, {
+      type: "text",
+      _class: "subheader",
+      content: "Total Project Cost"
+    }, {
+      type: "text",
+      _class: "",
+      content: "TBD (estimated at $100,000,000)"
+    }, {
+      type: "text",
+      _class: "subheader",
+      content: "Area"
+    }, {
+      type: "text",
+      _class: "",
+      content: "TBD (estimated 200,000 GSF)"
+    }, {
+      type: "image",
+      _class: "",
+      content: "/public/img/college_house_large_1.jpg"
+    }, {
+      type: "image",
+      _class: "",
+      content: "/public/img/college_house_large_2.jpg"
+    }, 
+    {
+      type: "image",
+      _class: "",
+      content: "/public/img/new-college-house-Chestnut-view-aerial.jpg"
+    }, 
+    {
+      type: "image",
+      _class: "",
+      content: "/public/img/new-college-house-Chestnut-view-aerial.jpg"
+    }, 
+    {
+      type: "image",
+      _class: "",
+      content: "/public/img/new-college-house-aerial-google-earth.jpg"
+    }, 
+    {
+      type: "image",
+      _class: "",
+      content: "/public/img/new-college-house-top-right-01.jpg"
+    }, {
+      type: "location",
+      _class: "",
+      content: [39.953067, -75.190902]
+    }
+  ]
+};
+
+var plan2 = {
+  title: "Translational Research Center, School of Medicine",
+  image_thumbnail: "/public/img/college_house_thumb.jpg",
+  supporters: [],
+  proposals: [],
+  elements: [
+    {
+      type: "text",
+      _class: "subheader",
+      content: "Planning Strategy"
+    }, {
+      type: "text",
+      _class: "",
+      content: "The New College House will establish a gateway to campus, as it is located at a key pedestrian approach from the northeastern corner of the University. It will be the first building one encounters coming from the 30th Street Amtrak and Regional Rail Station, and will front onto Woodland Walk, one of the three main pedestrian paths identified in the Campus Development Plan. A new public open space or commons will enliven the center of this important residential quadrangle."
+    }, {
+      type: "text",
+      _class: "subheader",
+      content: "Program"
+    }, {
+      type: "text",
+      _class: "",
+      content: "In addition to biomedical laboratories, the building will include clinical/patient-oriented research facilities, imaging, offices, and supportive research space."
+    }, {
+      type: "text",
+      _class: "subheader",
+      content: "Total Project Cost"
+    }, {
+      type: "text",
+      _class: "",
+      content: "$261,000,000"
+    }, {
+      type: "text",
+      _class: "subheader",
+      content: "Area"
+    }, {
+      type: "text",
+      _class: "",
+      content: "500,000 GSF"
+    }, {
+      type: "image",
+      _class: "",
+      content: "/public/img/conference-room-glow-03.png"
+    }, {
+      type: "image",
+      _class: "",
+      content: "/public/img/conference-room-table-view-01.jpg"
+    }, 
+     {
+      type: "image",
+      _class: "",
+      content: "/public/img/conference-room-02.jpg"
+    }, 
+     {
+      type: "image",
+      _class: "",
+      content: "/public/img/conference-room-table-view-02.jpg"
+    }, 
+     {
+      type: "image",
+      _class: "",
+      content: "/public/img/conference-room-sketch-people-03.png"
+    }, {
+      type: "location",
+      _class: "",
+      content: [39.948043,-75.193416]
+    }
+  ]
+};
